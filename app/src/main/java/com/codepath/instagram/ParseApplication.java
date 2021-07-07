d1bd3c5906fbcb4720116fea4eb4bac8ab3df8ca
sahanan13 <sahanahome@gmail.com>
2021-07-07 10:37:31 -0700

    User can login to their account, and current signed in user is persisted across app restarts

---
+++ app/src/main/java/com/codepath/instagram/ParseApplication.java
@@ -0,0 +1,22 @@
+package com.codepath.instagram;
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+public class ParseApplication extends Application {
+
+    // Initializes Parse SDK as soon as the application is created
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Register your parse models
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("TlY3TERiJVRcNKyYklK3XjSBwmJY6UNfKuhrIFQ5")
+                .clientKey("IOdYpBK4KVS9A73L9C5Q6Qwsr4HPlgORIlNEZhTf")
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+    }
+}
